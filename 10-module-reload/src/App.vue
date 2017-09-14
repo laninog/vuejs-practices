fb66676234c42898fc142abcab6dc1b7f7b45729
Alejandro Niño <alejandro6230@gmail.com>
2017-09-14 23:52:58 +0200

    Vuejs module reload
    - CSS Scoped
    - Hot module reload

---
+++ 10-module-reload/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div>
+
+        <movies></movies>
+
+        <hr>
+
+        <h2>Out CSS Scope</h2>
+
+        <ul>
+            <li><a href="#">Test Scope</a></li>
+        </ul>
+
+    </div>
+
+</template>
+
+<script>
+    import movies from './components/movies.vue';
+
+    export default {
+        components: {movies}
+    }
+</script>
---
+++ 10-module-reload/src/components/movies.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h2>Movies</h2>
+        <ul>
+            <li v-for="m in pendingMovies">
+                <a href="#" @click="updateMovie(m)" v-text="m.name"></a>
+            </li>
+        </ul>
+        <h2>Movies Watched</h2>
+        <ul>
+            <li v-for="m in watchedMovies">
+                <a href="#" @click="updateMovie(m)" v-text="m.name"></a>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                movies: [
+                    {name: '2012', watched: false},
+                    {name: 'I am Legend', watched: false},
+                    {name: 'The Big Short', watched: false},
+                    {name: 'Elektra', watched: false},
+                ],
+                finished: []
+            }
+        },
+        methods: {
+            updateMovie(movie) {
+                movie.watched = !movie.watched;
+            }
+        },
+        computed: {
+            pendingMovies() {
+                return this.movies.filter((movie) => !movie.watched);
+            },
+            watchedMovies() {
+                return this.movies.filter((movie) => movie.watched);
+            }
+        }
+    }
+</script>
+<style scoped>
+    li a {
+        color: darkgoldenrod;
+        font-weight: bold;
+        text-decoration: none;
+    }
+</style>
